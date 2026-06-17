c20fa501b2cc756e9503d816003918ee45ed467b
Qazi Sultan <sultankhan121jm@gmail.com>
2026-06-17 18:35:41 +0500

    Add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap'; // <-- Added this
 
 export default defineConfig({
   site: 'https://qazi-sultan.vercel.app',
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), sitemap()], // <-- Added sitemap() here
   output: 'static',
   build: {
     inlineStylesheets: 'auto'
